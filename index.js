63c2f40492ef1a4208a6733bb18e298c3f0a2a5c
wahe7 <waheguru002200@gmail.com>
2023-09-11 00:28:07 +0530

    change printing method

--- index.js
+++ index.js
@@ -1,22 +1,78 @@
-let form=document.querySelector("#form");
+let form=document.querySelector(".form");
+let form1=document.querySelector(".form1");
+let form2=document.querySelector(".form2");
 
-form.addEventListener("submit",function(ev){
+form.addEventListener("click",function(ev){
    
     ev.preventDefault();
     let inp=document.querySelector(".input");
     let value=inp.value;
     let far=parseFloat(value)*9/5+32;
-    
-    convert(value,far);
-    inp.value="";
+    let p=document.querySelector(".input1");
+    p.value=far;
+    let kel=parseFloat(value)+273.15;
+    let k=document.querySelector(".input2");
+    kel=kel.toFixed(3);
+    k.value=kel;
    
 })
 
-function convert(value,far)
+
+
+
+
+
+form1.addEventListener("submit",function(ev){
+   
+    ev.preventDefault();
+    let inp1=document.querySelector(".input1");
+    let value1=inp1.value;
+    let cel=(parseFloat(value1)-32)*5/9;
+    let cel1=cel.toFixed(3);
+    
+    let t=document.querySelector(".input");
+    t.value=cel1;
+    let k=document.querySelector(".input2");
+    let kel=(parseFloat(value1)-32)*5/9+273.15;
+    kel=kel.toFixed(3);
+    k.value=kel;
+
+   
+   
+})
+
+
+
+form2.addEventListener("submit",function(ev){
+   
+    ev.preventDefault();
+    let inp2=document.querySelector(".input2");
+    let value2=inp2.value;
+    let cel=(parseFloat(value2)-273.15);
+   let c=document.querySelector(".input");
+   cel=cel.toFixed(3);
+   c.value=cel;
+    
+    let t=document.querySelector(".input1");
+    let f=(parseFloat(value2)-273.15)*9/5+32;
+    f=f.toFixed(3);
+    t.value=f;
+   
+   
+   
+})
+let clr=document.querySelector(".clear");
+clr.addEventListener("click",function(ev)
 {
-    let p=document.querySelector(".print");
-    p.innerHTML=`<h1>${value} °C = ${far} °F`;
-}
+    ev.preventDefault();
+    let inp=document.querySelector(".input");
+    let inp1=document.querySelector(".input1");
+    let inp2=document.querySelector(".input2");
+    inp.value="";
+    inp1.value="";
+    inp2.value="";
+
+})
 
 
 
